30070a9bc55801770aa79fd742ab3c3940554d6e
nqhuyD <huynguyennbk@gmail.com>
2019-06-03 21:54:47 +0700

    config apollo client graphql

---
+++ client/components/authenticate/signup.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { CommonInput, CommonButton } from '../common';
+
+const styles = {
+  styleContainer: {
+    flex: 0.7,
+    marginTop: 150,
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+};
+
+export const SignUpScreen = () => (
+  <View style={styles.styleContainer}>
+    <CommonInput placeholder="User Name" />
+    <CommonInput placeholder="Email" />
+    <CommonInput placeholder="Password" />
+    <CommonInput placeholder="Confirmation" />
+    <CommonButton>Sign Up</CommonButton>
+  </View>
+);
---
+++ client/provider/withApolloProvider.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+
+const client = new ApolloClient({
+  link: createHttpLink({ uri: 'http://localhost:8000/graphql' }),
+  cache: new InMemoryCache(),
+});
+
+export const withApolloProvider = (Component) => () => (
+  <ApolloProvider client={client}>
+    <Component />
+  </ApolloProvider>
+);
